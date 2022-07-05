68c27738352478948dcfdb0f988ba99e99be30af
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2022-07-05 17:46:05 +0300

    Updated web application. Now user can download output file

--- web_app.py
+++ web_app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+from io import BytesIO
+
+from flask import Flask, render_template, request, send_file, session
 
 import parser
 
@@ -13,9 +15,20 @@ def hello():
 @app.route('/', methods=['POST'])
 def my_form_post():
     url = request.form['url']
+    session['url'] = url
     number_of_signatures = parser.get_number_of_signatures_for_petition(url)
-    return "There are {} signatures for this petition".format(number_of_signatures)
+    return render_template('output.html', number_of_signatures=number_of_signatures)
+
+
+@app.route('/download')
+def download():
+    url = session.get('url', None)
+    petition_code = url.split('/')[-1]
+    json = parser.get_signatories_for_petition(url)
+    json_io = BytesIO(json.getvalue().encode('utf8'))
+    return send_file(json_io, download_name=f'signatories_{petition_code}.json', mimetype='json', as_attachment=True)
 
 
 if __name__ == '__main__':
+    app.secret_key = 'key'
     app.run()
