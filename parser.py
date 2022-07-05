c3f6d70e5201feb6e847b31e4b8b0355a76dd71e
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2022-07-05 17:45:32 +0300

    Updated module to work with urls

--- parser.py
+++ parser.py
@@ -1,13 +1,12 @@
 import json
+from io import StringIO
 
 import bs4
 import cloudscraper
 
 
-def get_signatories_for_petition(petition_code: str):
+def get_signatories_for_petition(petition_url: str):
     scraper = cloudscraper.create_scraper()
-
-    petition_url = f'https://petition.president.gov.ua/petition/{petition_code}'
 
     page_number = 1
     names_list = []
@@ -29,7 +28,9 @@ def get_signatories_for_petition(petition_code: str):
 
         page_number += 1
 
-    return names_list
+    json_io = StringIO()
+    json.dump(names_list, json_io, ensure_ascii=False, indent=4)
+    return json_io
 
 
 def get_number_of_signatures_for_petition(petition_url: str):
@@ -43,9 +44,3 @@ def get_number_of_signatures_for_petition(petition_url: str):
     number_of_signatures = int(soup.find('div', {'class': 'petition_votes_txt'}).span.text)
 
     return number_of_signatures
-
-print(get_number_of_signatures_for_petition('https://petition.president.gov.ua/petition/145004'))
-# list_of_signatories = get_signatories_for_petition('141740')
-#
-# with open("signatories_141740.json", "w", encoding='utf-8') as final:
-#     json.dump(list_of_signatories, final, sort_keys=False, ensure_ascii=False, indent=4)
